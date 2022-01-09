8a7a980ae9b86ee9d1f977f713136b0bac878f04
CodingOwl1 <flyingowls12@gmail.com>
2022-01-08 22:06:25 -0500

    stuff

---
+++ client/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    semi: 'off',
+    'comma-dangle': 'off',
+    'no-shadow': 'off',
+    'react-native/no-inline-styles': 'off'
+  }
+}
